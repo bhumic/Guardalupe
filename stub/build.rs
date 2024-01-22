5b72cdc82b302ce1721dd631a92c8315d7e22958
bhumic <humic.bruno@gmail.com>
2024-01-22 22:40:59 +0100

    Link with Luajit

---
+++ stub/build.rs
@@ -0,0 +1,4 @@
+fn main() {
+    println!(r"cargo:rustc-link-search=E:\repository\Guardalupe\stub\libs");
+    println!(r"cargo:rustc-link-lib=static=lua51");
+}
--- stub/src/main.rs
+++ stub/src/main.rs
@@ -1,3 +1,15 @@
+use libc::c_void;
+
+extern "C" {
+    fn luaL_newstate() -> *mut c_void;
+    fn luaL_openlibs(L: *mut c_void) -> *mut c_void;
+    fn lua_close(L: *mut c_void) -> *mut c_void;
+}
+
 fn main() {
-    println!("Hello, world!");
+    unsafe {
+        let l = luaL_newstate();
+        luaL_openlibs(l);
+        lua_close(l);
+    }
 }
